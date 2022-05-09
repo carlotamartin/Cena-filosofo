6903f47bd230180cdcd2463429d2ea4f5e0033be
Carlota Martin-Anero <91574355+carlotamartin@users.noreply.github.com>
2022-05-09 10:21:09 +0000

    eliminamos cosas

--- ventana.py
+++ ventana.py
@@ -23,32 +23,3 @@ class window ():
   def run(self):
     self.window.mainloop()
   
-    
-    
-
-'''
-
-class Ventana():
-    def __init__(self,titulo,color="light steel blue",geometria="537x250"):
-        self.window = tk.Tk()
-        self.title(titulo)
-        self.geometry(geometria)
-        self.
-        self.Text(self.window, height=30,width=80)
-   
-        self.Scrollbar(self.window)
-        self.texto.configure(yscrollcommand= self.scroll.set)
-        self.texto.pack(side= tk.LEFT)
-        self.scroll.config(command= self.texto.yview)
-        self.scroll.pack(side= tk.RIGHT,fill= tk.Y)
-
-    def visualización(self,texto):
-        self.texto.insert(tk.END, str(texto)+"\n")
- 
-    def ocultar(self):
-        self.withdraw()
-    def mostrar(self):
-        self.deiconify()
-    def run(self):
-      self.window.mainloop()
-'''
